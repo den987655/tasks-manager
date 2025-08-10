ca92fcfd4fdb7aa1be0d8a722af11d173d51e930
den987655 <36845126+den987655@users.noreply.github.com>
2025-08-10 16:34:34 +0300

    Add files via upload

---
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -0,0 +1,46 @@
+import { TaskStatus } from "@/common/enums"
+import { useGetTasksQuery } from "@/features/todolists/api/tasksApi"
+import type { DomainTodolist } from "@/features/todolists/lib/types"
+import { TasksPagination } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination.tsx"
+import List from "@mui/material/List"
+import { useState } from "react"
+import { TaskItem } from "./TaskItem/TaskItem"
+import { TasksSkeleton } from "./TasksSkeleton/TasksSkeleton"
+
+type Props = {
+  todolist: DomainTodolist
+}
+
+export const Tasks = ({ todolist }: Props) => {
+  const { id, filter } = todolist
+  const [page, setPage] = useState(1)
+
+  const { data, isLoading, isFetching } = useGetTasksQuery({
+    todolistId: id,
+    params: { page },
+  })
+  console.log({ isLoading, isFetching })
+
+  let filteredTasks = data?.items
+  if (filter === "active") {
+    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New)
+  }
+  if (filter === "completed") {
+    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.Completed)
+  }
+
+  if (isLoading) {
+    return <TasksSkeleton />
+  }
+
+  return (
+    <>
+      {filteredTasks?.length === 0 ? (
+        <p>Тасок нет</p>
+      ) : (
+        <List>{filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist} />)}</List>
+      )}
+      <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
+    </>
+  )
+}
